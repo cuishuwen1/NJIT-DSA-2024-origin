a7bb90acb974cd08bcebb1c5c34d051307546854
Stephen <1596880264@qq.com>
2024-03-26 09:43:26 +0800

    rewrite

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -12,26 +12,12 @@ public class Grades {
    }
 
    public void reverse() {
-      int i = 0;
-      while (i < grades.length/2) {
-         Integer temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-      }
+      Algorithms.reverse(this.grades);
    }
 
    public void sort() {
-      int n = grades.length;
-      for (int j = 1; j < n; j++) {
-         int key = grades[j];
-         int i = j - 1;
-         while (i >= 0 && grades[i] > key) {
-            grades[i + 1] = grades[i];
-            i = i - 1;
-         }
-         grades[i + 1] = key;
-      }
+      Algorithms.sort(this.grades);
+
    }
 
    public Integer [] getArray() {

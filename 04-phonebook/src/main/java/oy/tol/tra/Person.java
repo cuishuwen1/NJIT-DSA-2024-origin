b2bf9e34d8b10140a091ce55d538229680628c91
Stephen <1596880264@qq.com>
2024-04-02 23:05:08 +0800

    1

--- 04-phonebook/src/main/java/oy/tol/tra/Person.java
+++ 04-phonebook/src/main/java/oy/tol/tra/Person.java
@@ -61,10 +61,12 @@ public class Person implements Comparable<Person> {
      */
     @Override
     public int hashCode() {
-        int hash = 5381;
-        hash = (hash << 5) + hash ^ (firstName != null ? firstName.hashCode() : 0);
-        hash = (hash << 5) + hash ^ (lastName != null ? lastName.hashCode() : 0);
-        return hash & 0x7fffffff;
+        int hash=0;
+        String hashString=firstName+lastName;
+        for (int i = 0; i < hashString.length(); i++) {
+            hash=37*hash+hashString.charAt(i);
+        }
+        return hash;
     }
 
     /**
